35dcff99df67995159cc20107f20a9013b75b847
ArmanM26 <armanmkrtchyan260612@gmail.com>
2025-08-05 17:12:33 +0400

    Initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // if you're using a plain HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // for all your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
